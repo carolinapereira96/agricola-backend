b906a9199e1a93cad7cc56ce00df0f4183f22c25
carolinapereira96 <68658418+carolinapereira96@users.noreply.github.com>
2021-05-02 14:00:30 -0400

    agregar cross a los controller

--- src/main/java/com/agricola/backend/controllers/AdministradorRestController.java
+++ src/main/java/com/agricola/backend/controllers/AdministradorRestController.java
@@ -2,6 +2,7 @@ package com.agricola.backend.controllers;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.agricola.backend.models.entity.Administrador;
 import com.agricola.backend.models.services.IAdministradorService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class AdministradorRestController {
